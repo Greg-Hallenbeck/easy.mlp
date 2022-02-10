c30f7fbfa9b879adbc132c4d248f37acf8350f53
Greg Hallenbeck <greg.hallenbeck@gmail.com>
2022-02-10 00:35:56 +0000

    Fixed errors when predicting new data.

--- R/.ipynb_checkpoints/split.dataset-checkpoint.R
+++ R/.ipynb_checkpoints/split.dataset-checkpoint.R
@@ -4,40 +4,51 @@
 #' @return A list containing the design matrix (X), and the Y values (one-hot encoded as necessary).
 #'     If Y is a factor, then raw.Y is still factors, and levels contains the levels that the factor can take.
 split.dataset <- function(formula, data)
-{
-    # Pre-process and create a training X and Y datasets
-    # This code has been taken from the lm() function
-    # (which has been released under the GPL 2)
+{    
+    # This function also gets called when Y is not defined in the data
+    # (e.g. when running the model on new data or testing data)
+    # This code checks for that and adds a dummy Y full of 1s.
+    yvar = as.character(formula[[2]])
+    if (! (yvar %in% names(data)) )
+        data[,yvar] = 1
     
-    # At its end, dataset is a table where column 1 = Y, and the remaining columns are X
-    mf <- match.call(expand.dots = FALSE)
-    m <- match(x = c("formula", "data"), table = names(mf), nomatch = 0L)
-    mf <- mf[c(1L, m)]
-    mf$drop.unused.levels <- TRUE
-    mf[[1L]] <- quote(stats::model.frame)
-    #dataset <- eval(expr = mf, envir = parent.frame())
-    mf <- eval(expr = mf, envir = parent.frame())
-    mt <- attr(mf, "terms") # allow model.frame to update it
-    dataset <- model.matrix(mt, mf)
+    # Make a new dataframe only including Y and X variables, in that order.
+    dataset <- model.frame(formula, data=data)
     
     # Split out the X and Y variables
-    
-    raw.Y = mf[,1] # Split out the Y variable
+    raw.Y = dataset[,1] # Split out the Y variable
     Y = mltools::one_hot(data.table::as.data.table(raw.Y)) # one-hot encode the Y, if its a factor
     Y = t(as.matrix(Y)) # Convert to a row vector
     rownames(Y) = NULL # Strip the column and row names from the matrix
     colnames(Y) = NULL
-    
-    X = dataset[,2:ncol(dataset)] # Split out the X variables
-    #X = mltools::one_hot(data.table::as.data.table(X)) # one-hot encode any factors
-    X = t(as.matrix(X)) # Convert to a row vector (each column is one observation)
-    rownames(X) = NULL # Strip the column and row names from the matrix
-    colnames(X) = NULL
 
     # If the dataset has factors, make sure to store what they are
     # so that when making predictions later, we can decode them
     if (is.factor(raw.Y))
         levels = factor(levels(raw.Y), levels(raw.Y))
+                    
+    X = dataset[,2:ncol(dataset)] # Split out the X variables
+    X = mltools::one_hot(data.table::as.data.table(X)) # one-hot encode any factors
+    X = t(as.matrix(X)) # Convert to a row vector (each column is one observation)
+    rownames(X) = NULL # Strip the column and row names from the matrix
+    colnames(X) = NULL
     
     return(list(X=X, Y=Y, raw.Y=raw.Y, levels=levels))
 }
+
+    # Pre-process and create a training X and Y datasets
+    # This code has been taken from the lm() function
+    # (which has been released under the GPL 2)
+    
+    # At its end, dataset is a table where column 1 = Y, and the remaining columns are X
+    #mf <- match.call(expand.dots = FALSE)
+    #m <- match(x = c("formula", "data"), table = names(mf), nomatch = 0L)
+    #print(m)
+    #mf <- mf[c(1L, m)]
+    #mf$drop.unused.levels <- TRUE
+    #mf[[1L]] <- quote(stats::model.frame)
+    #dataset <- eval(expr = mf, envir = parent.frame())
+    #mf <- eval(expr = mf, envir = parent.frame())
+    #print(mf)
+    #mt <- attr(mf, "terms") # allow model.frame to update it
+    #dataset <- model.matrix(mt, mf)
--- R/predict.mlp.R
+++ R/predict.mlp.R
@@ -3,15 +3,17 @@
 #' @param newdata An object of class `data.frame` with the same input columns as the data frame used to create the network.
 #'    If left NULL, the training data will be used.
 #'    If set to "valid", the validation data will be used.
-#' @param type If set to "numeric", then predicted numeric values (regression), probabilities (logistic), or a matrix of probability for each class (classification) is returned. For a classification network, type="labels" can be used to return the class with the highest probability.
+#' @param type What should be returned. If set to "numeric", then predicted numeric values (regression), probabilities (logistic), or a matrix of probability for each class (classification) is returned. For a classification network, type="labels" can be used to return the class with the highest probability.
 #' @return A vector (or matrix, for a classification network) of the predicted outputs.
 #' @examples
 #'    predict(network, type="labels")
 #'    predict(network, newdata="valid", type="labels")
 #'    predict(network, newdata=test.data)
 #' @export
-predict.mlp <- function(network, newdata="train", type="numeric")
+predict.mlp <- function(network, newdata="train", type=NULL)
 {
+    one.row = FALSE
+    
     # Does the user want to use the training data?
     if (is.null(newdata) || newdata == "train")
     {
@@ -20,6 +22,14 @@ predict.mlp <- function(network, newdata="train", type="numeric")
     # Is new data provided? Process it into a matrix and apply normalization
     else if (is.data.frame(newdata))
     {
+        # Check if there's only a single entry in the data frame
+        # This creates an error as R keeps re-casting 1D matrices as vectors.
+        if (nrow(newdata) == 1)
+        {
+            one.row = TRUE
+            newdata = rbind(newdata, newdata)
+        }
+        
         newdata = split.dataset(network$formula, newdata)$X
         newdata = (newdata - network$train$xbar)/network$train$sigma
     }
@@ -44,6 +54,14 @@ predict.mlp <- function(network, newdata="train", type="numeric")
         index = apply(Y, 2, which.max)
         # Convert that into one of the labels
         Y = network$levels[index]
+    }
+    
+    if (one.row)
+    {
+        if (network$type == "classification" & type == "numeric")
+            return(Y[,1])
+        else
+            return(Y[1])
     }
     
     return(Y)
